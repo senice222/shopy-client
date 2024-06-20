212d726197c41621dc1d93b4704303f85ac0a26a
senice222 <artemprimorksij7@gmail.com>
2024-06-20 14:14:02 +0300

    more work on admin panel

---
+++ src/components/ADMIN/FastMoves/FastMoves.tsx
@@ -0,0 +1,56 @@
+import style from './FastMoves.module.scss'
+import users from "../../../assets/users-01.png";
+import bag from "../../../assets/shopping-bag-02.png";
+import orders from "../../../assets/receipt.png";
+import rassylka from "../../../assets/message-text-square-01.png";
+
+const FastMoves = () => {
+    return (
+        <div className={style.fastMoves}>
+            <h2 className={style.title}>Быстрые действия</h2>
+            <div className={style.items}>
+                <div className={style.item}>
+                    <div className={style.icon}>
+                        <img src={users} alt="/"/>
+                    </div>
+                    <div className={style.users}>
+                        <h2>Пользователи</h2>
+                        <p>Список и управление пользователями</p>
+                    </div>
+                </div>
+                {/*/*/}
+                <div className={style.item}>
+                    <div className={style.icon}>
+                        <img src={bag} alt="/"/>
+                    </div>
+                    <div className={style.users}>
+                        <h2>Категории товаров</h2>
+                        <p>Изменяйте и добавляйте товары</p>
+                    </div>
+                </div>
+                {/*/*/}
+                <div className={style.item}>
+                    <div className={style.icon}>
+                        <img src={orders} alt="/"/>
+                    </div>
+                    <div className={style.users}>
+                        <h2>Заказы</h2>
+                        <p>Список и управление заказами</p>
+                    </div>
+                </div>
+                {/*/*/}
+                <div className={style.item}>
+                    <div className={style.icon}>
+                        <img src={rassylka} alt="/"/>
+                    </div>
+                    <div className={style.users}>
+                        <h2>Рассылки</h2>
+                        <p>Создавайте и рассылайте сообщения</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FastMoves;
--- src/pages/ADMIN/Panel/Panel.tsx
+++ src/pages/ADMIN/Panel/Panel.tsx
@@ -1,5 +1,8 @@
 import AdminLayout from "../../../layouts/AdminLayout";
 import style from './Panel.module.scss'
+import FastMoves from "../../../components/ADMIN/FastMoves/FastMoves";
+import Button from "../../../components/Button/Button";
+import reverse from '../../../assets/reverse-left.png'
 
 const Panel = () => {
     return (
@@ -23,9 +26,237 @@ const Panel = () => {
                             <h2>1,322</h2>
                         </div>
                     </div>
-                    <div className={style.fastMoves}>
-                        <h2>Быстрые действия</h2>
-
+                    <FastMoves/>
+                </div>
+                <div className={style.wrapper}>
+                    <div className={style.lastOrders}>
+                        <div className={style.orderHeader}>
+                            <h2>Последние заказы</h2>
+                            <Button text={"Перейти"} height={"40px"} width={"92px"} fontSize={"14px"}/>
+                        </div>
+                        <div className={style.container}>
+                            <div className={style.scrollContainer}>
+                                <table className={style.ordersTable}>
+                                    <thead>
+                                    <tr>
+                                        <th>Заказ</th>
+                                        <th>Товар</th>
+                                        <th>Статус</th>
+                                        <th>Действия</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td className={style.id}>#3066</td>
+                                        <td>Monthly subscription</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>#3065</td>
+                                        <td>Monthly subscription</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>#3064</td>
+                                        <td>Monthly subscription</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>#3063</td>
+                                        <td>Monthly subscription</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>#3062</td>
+                                        <td>Monthly subscription</td>
+                                        <td className={`${style.status} ${style.refunded}`}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.refunded}>
+                                                    <img src={reverse} alt="/"/>
+                                                    <p>Refunded</p>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>#3061</td>
+                                        <td>Monthly subscription</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    {/*<tr>*/}
+                                    {/*    <td className={style.id}>#3061</td>*/}
+                                    {/*    <td>Monthly subscription</td>*/}
+                                    {/*    <td className={style.status}>*/}
+                                    {/*        <div className={style.wrapperPaid}>*/}
+                                    {/*            <div className={style.paid}>✔ Paid</div>*/}
+                                    {/*        </div>*/}
+                                    {/*    </td>*/}
+                                    {/*    <td className={style.actions}>*/}
+                                    {/*        <span className={style.info}>ℹ</span>*/}
+                                    {/*        <span className={style.edit}>✎</span>*/}
+                                    {/*    </td>*/}
+                                    {/*</tr>*/}
+                                    {/*<tr>*/}
+                                    {/*    <td className={style.id}>#3061</td>*/}
+                                    {/*    <td>Monthly subscription</td>*/}
+                                    {/*    <td className={style.status}>*/}
+                                    {/*        <div className={style.wrapperPaid}>*/}
+                                    {/*            <div className={style.paid}>✔ Paid</div>*/}
+                                    {/*        </div>*/}
+                                    {/*    </td>*/}
+                                    {/*    <td className={style.actions}>*/}
+                                    {/*        <span className={style.info}>ℹ</span>*/}
+                                    {/*        <span className={style.edit}>✎</span>*/}
+                                    {/*    </td>*/}
+                                    {/*</tr>*/}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.lastOrders}>
+                        <div className={style.orderHeader}>
+                            <h2>Последние пользователи</h2>
+                            <Button text={"Перейти"} height={"40px"} width={"92px"} fontSize={"14px"}/>
+                        </div>
+                        <div className={style.container}>
+                            <div className={style.scrollContainer}>
+                                <table className={style.ordersTable}>
+                                    <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Статус</th>
+                                        <th>Id</th>
+                                        <th>Действия</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td className={style.id}>Olivia Rhye</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td>1000103230</td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>Olivia Rhye</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td>1000103230</td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>Olivia Rhye</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td>1000103230</td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>Olivia Rhye</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td>1000103230</td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>Olivia Rhye</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td>1000103230</td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td className={style.id}>Olivia Rhye</td>
+                                        <td className={style.status}>
+                                            <div className={style.wrapperPaid}>
+                                                <div className={style.paid}>✔ Paid</div>
+                                            </div>
+                                        </td>
+                                        <td>1000103230</td>
+                                        <td className={style.actions}>
+                                            <span className={style.info}>ℹ</span>
+                                            <span className={style.edit}>✎</span>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
