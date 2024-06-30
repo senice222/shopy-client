40d0b9b5d8536d5579c517a65e1d1a6616ef047b
senice222 <artemprimorksij7@gmail.com>
2024-06-30 13:46:30 +0300

    searching id

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -14,13 +14,12 @@ const Basket = () => {
     const navigate = useNavigate()
     const {id} = useTelegram()
     const { data, error } = useSWR(`${url}/api/user/cart/${id}`, fetcher);
-    // alert(id)
+
     return (
         <div className={style.wrapp}>
             <Layout isRightArrow={true}>
                 <div className={style.basketContainer}>
                     <TopItem text={"Корзина"}/>
-                    {id ? id : "НЕТУ НАХУЙ"}
                     <div className={style.items}>
                         {data?.length > 0 ? (
                             data.map((item: BasketProps, i: number) => (
