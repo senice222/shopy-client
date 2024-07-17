870aaf1c75a69f3620751eaf594058b114397a30
senice222 <artemprimorksij7@gmail.com>
2024-07-17 16:16:11 +0300

    modal animation

--- src/components/Modals/Accounts/AddAccountModal/AddAccountModal.tsx
+++ src/components/Modals/Accounts/AddAccountModal/AddAccountModal.tsx
@@ -85,7 +85,7 @@ const AddAccountModal = ({addAccount, onClose}: AccountProps) => {
     };
 
     return (
-        <BootstrapModal Y={-70} isFocused={isFocused} onClose={onClose} active={addAccount}>
+        <BootstrapModal isFocused={isFocused} onClose={onClose} active={addAccount}>
             <div className={style.modalWinPopupHead}>
                 <h3>Добавить аккаунт</h3>
                 <svg
