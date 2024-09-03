e79aa7f122e09ad02b43580b0ef2161d0b4571d8
senice222 <sasdasd920@gmail.com>
2024-09-03 13:18:27 +0300

    url

--- src/core/fetch.ts
+++ src/core/fetch.ts
@@ -1,4 +1,4 @@
 
 //https://shopy-server-five.vercel.app
-export const url = 'http://localhost:4000'
+export const url = 'https://shopy-server-five.vercel.app'
 export const fetcher = (url: string, init?: RequestInit) => fetch(url, init).then(res => res.json())
